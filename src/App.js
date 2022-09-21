5927ea11605c15c32ab5b458607aa8e68f984626
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-09-21 18:18:13 +0600

    header updated

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import MyHome from './pages/MyHome/MyHome';
 import { Routes, Route } from "react-router-dom";
 import AboutMe from './pages/MyHome/AboutMe/AboutMe';
 import Projects from './pages/MyHome/Projects/Projects';
+import MyHeader from './pages/Shared/MyHeader/MyHeader';
 
 
 function App() {
   return (
     <div >
+      <MyHeader></MyHeader>
       <Routes>
         <Route path="/" element={<MyHome />} />
         <Route path="home" element={<MyHome />} />
--- src/pages/MyHome/MyHome.js
+++ src/pages/MyHome/MyHome.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MyHeader from '../Shared/MyHeader/MyHeader';
+
 import AboutMe from './AboutMe/AboutMe';
 import MyBanner from './MyBanner/MyBanner';
 import Projects from './Projects/Projects';
@@ -8,7 +8,7 @@ import Skills from './Skills/Skills';
 const MyHome = () => {
     return (
         <div>
-            <MyHeader></MyHeader>
+
             <MyBanner></MyBanner>
 
             <Skills></Skills>
